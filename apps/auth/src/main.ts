b2ec02eedfdb8fbb3d9980d7b6ba575aca3263ef
esrichardd <espinozar1994@gmail.com>
2023-06-13 22:13:30 -0400

    feat(users,auth): add libraries to services

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { AppModule } from './app.module';
+import { LoggerService } from '@libs/logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+
+  const logger = new LoggerService();
+  logger.log(`auth API is running on port ${port}`, 'auth');
+  app.setGlobalPrefix('auth');
+  await app.listen(port);
 }
 bootstrap();
